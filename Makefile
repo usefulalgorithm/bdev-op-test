6e977e645e12ec17f3b58d1e70261905165b72a4
Andy Lii <usefulalgorithm@gmail.com>
2018-12-05 19:39:43 +0800

    Get basic ssd info

--- Makefile
+++ Makefile
@@ -1,11 +1,12 @@
 TARGET = bdev-op-test
 CC = g++
-CFLAGS = -g -O3 -Wall
+INCLUDE = include
+CFLAGS = -g -O0 -Wall -std=c++11
 
 all: $(TARGET)
 $(TARGET): main.o
 	$(CC) -o $(TARGET) main.o
 main.o: main.cc
-	$(CC) -c main.cc
+	$(CC) -I$(INCLUDE) $(CFLAGS) -c main.cc
 clean:
 	@rm -f $(TARGET) *.o
--- main.cc
+++ main.cc
@@ -1,9 +1,63 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
-#include <unistd.h>
+#include <cstring>
+#include <cerrno>
+#include "fcntl.h"
+#include "unistd.h"
+#include <sys/ioctl.h>
+#include <linux/fs.h>
+
+using std::string;
+using std::cout;
+using std::cerr;
+using std::endl;
+
+char buffer[512];
+string ssd_devname("/dev/sdf2");
+
+void usage(string pname) {
+  cerr << "Usage: " << pname << endl;
+  exit(EXIT_FAILURE);
+}
 
 int main(int argc, char* argv[]) {
-  std::cout << "Hello world!" << std::endl;
-  return EXIT_SUCCESS;
+  string pname = argv[0];
+  // default ssd location
+  int opt, ssd_fd;
+  int ssd_dev_size;
+  int ssd_sector_size;
+  while ((opt = getopt(argc, argv, "s:")) != -1) {
+    switch (opt) {
+      case 's':
+        ssd_devname = optarg;
+        break;
+      default:
+        usage(pname);
+    }
+  }
+  ssd_fd = open(ssd_devname.c_str(), O_RDONLY);
+  if (ssd_fd < 0) {
+    cerr << "Cannot open " << ssd_devname << ": " << strerror(errno) << endl;
+    exit(EXIT_FAILURE);
+  }
+  lseek(ssd_fd, 0, SEEK_SET);
+  if (read(ssd_fd, buffer, 512) < 0) {
+    cerr << "Cannot read SSD superblock " << ssd_devname << ": " << strerror(errno) << endl;
+    exit(EXIT_FAILURE);
+  }
+  if (ioctl(ssd_fd, BLKGETSIZE, &ssd_dev_size) < 0) {
+    cerr << "Cannot get SSD device size: " << strerror(errno) << endl;
+    exit(EXIT_FAILURE);
+  }
+  if (ioctl(ssd_fd, BLKSSZGET, &ssd_sector_size) < 0) {
+    cerr << "Cannot get SSD sector size: " << strerror(errno) << endl;
+    exit(EXIT_FAILURE);
+  }
+  cout << ssd_dev_size << endl;
+  cout << ssd_sector_size << endl;
+  /*
+  cout << ssd_devname << ": device size = " << ssd_dev_size << ", sector size = " << ssd_sector_size << endl;
+  */
+  return 0;
 }
