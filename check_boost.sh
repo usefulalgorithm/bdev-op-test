4386605d051233b996473beb2ea61dc9ec3af9f8
Andy Lii <usefulalgorithm@gmail.com>
2018-12-13 18:05:39 +0800

    boost installer checks for targets

--- check_boost.sh
+++ check_boost.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
 
 PKG_ROOT=$PWD
-BOOST_VERSION="1_69_0"
-BOOST_NAME=boost_$BOOST_VERSION
+TARGETS="iostreams"
+BOOST_VERSION_NUM="1.69.0"
+BOOST_VERSION_STR="1_69_0"
+BOOST_NAME=boost_$BOOST_VERSION_STR
 BOOST_PATH=$PKG_ROOT/boost
 BOOST_TARBALL=$BOOST_PATH/"$BOOST_NAME".tar.bz2
+BOOST_TARGETS=""
 
-echo -n "Checking if boost exists... "
-if [ -d $BOOST_PATH/$BOOST_NAME/bin.v2 ] && [ -d $PKG_ROOT/include/boost ]; then
+check_target() {
+  target=$1
+  if [ ! -f $PKG_ROOT/lib/libboost_"$target".a ] || [ ! -f $PKG_ROOT/lib/libboost_"$target".so ]; then
+    echo "false"
+  else
+    echo "true"
+  fi
+}
+
+check_all_targets() {
+  for target in $TARGETS; do
+    if [ $(check_target $target) == "false" ]; then
+      echo "false"
+      return 0
+    fi
+  done
+  echo "true"
+}
+
+echo -n "Checking if boost is built... "
+if [ -d $BOOST_PATH/$BOOST_NAME/bin.v2 ] && [ -d $PKG_ROOT/include/boost ] && [ $(check_all_targets) == "true" ]; then
   echo "yes"
 else
   echo "no"
@@ -18,7 +40,7 @@ else
       echo "yes"
     else
       echo "no"
-      wget -P $BOOST_PATH https://dl.bintray.com/boostorg/release/1.69.0/source/boost_1_69_0.tar.bz2
+      wget -P $BOOST_PATH https://dl.bintray.com/boostorg/release/$BOOST_VERSION_NUM/source/"$BOOST_VERSION_STR".tar.bz2
     fi
 
     echo -n "Checking if SHA256 of boost tarball matches... "
@@ -28,7 +50,7 @@ else
     else
       echo "no"
       rm $BOOST_TARBALL
-      wget -P $BOOST_PATH https://dl.bintray.com/boostorg/release/1.69.0/source/boost_1_69_0.tar.bz2
+      wget -P $BOOST_PATH https://dl.bintray.com/boostorg/release/$BOOST_VERSION_NUM/source/"$BOOST_VERSION_STR".tar.bz2
     fi
 
     echo -n "Decompressing tarball... "
@@ -38,7 +60,10 @@ else
 
   cd $BOOST_PATH/$BOOST_NAME
   ./bootstrap.sh
-  ./b2 install --with-iostreams --prefix="$PKG_ROOT"
+  for target in $TARGETS; do
+    BOOST_TARGETS=$BOOST_TARGETS" --with-$target"
+  done
+  ./b2 install -d0 $BOOST_TARGETS --prefix="$PKG_ROOT"
 fi
 cd $PKG_ROOT
 echo "Done."
