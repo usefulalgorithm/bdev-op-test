2259de4af807623c44f966addd1b8587fcb48269
Andy Lii <usefulalgorithm@gmail.com>
2018-12-19 17:35:41 +0800

    add license

--- main.cc
+++ main.cc
@@ -1,3 +1,23 @@
+/*
+ *  bdev-op-test: A simple implementation of a block-based cache
+ *  Copyright (C) 2018  Tsung-Ju Lii <usefulalgorithm@gmail.com>
+ *
+ *  This file is part of bdev-op-test.
+ *
+ *  bdev-op-test is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  bdev-op-test is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with bdev-op-test.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include <boost/iostreams/device/file_descriptor.hpp>
 #include <boost/iostreams/stream.hpp>
 
